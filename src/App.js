5d4e6df1499c2d3658688fc089d2cd520a6137f4
Tanoshikatta <austinalexanderspears@gmail.com>
2018-09-28 00:17:10 -0400

    Testing out LinkContainer from react-router-bootstrap

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 //import { Carousel } from 'react-bootstrap';
 //import { Grid, Row, Col } from 'react-bootstrap';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
-import { BrowserRouter, Route } from 'react-router-dom'
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { LinkContainer } from 'react-router-bootstrap';
+
 import './App.css';
 import Home from './components/home';
 import Skills from './components/skills';
@@ -12,64 +14,41 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
-          {/* <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">About Me</h1>
-          </header> */}
-          <div>
-            {/* <Navbar> */}
-              {/* <Navbar.Header>
-                <Navbar.Brand>
-                   Austin Spears
-                </Navbar.Brand>
-              </Navbar.Header> */}
-              <Nav stacked>
-                <NavItem href="/">Home</NavItem>
-                <NavItem href="/skills">Skills</NavItem>
-              </Nav>
-            {/* </Navbar> */}
-
-            <Route exact path="/" component={Home}/>
-            <Route path="/skills" component={Skills}/>
-
-          {/* <Carousel className="Carousel" interval={null}>
-            <Carousel.Item className="CarouselItem">
-                <div className="CarDiv">
-                  <p>Software Engineer and team leader with extensive experience across the entire <br/>application development lifecycle.  Skilled at communicating with and interfacing <br/>between technical and non-technical parties.  Passionate about elegant algorithm and architecture design.</p>
-                </div>
-              <Carousel.Caption>
-                <h3>Summary</h3>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item className="CarouselItem">
-              <Grid className="CarGrid">
-                <Col md={6}>
-                  <ul>
-                    <li>Leadership</li>
-                    <li>C#, XAML, SQL</li>
-                    <li>Algorithm Design</li>
-                    <li>UX Design</li>
-                    <li>Architecture Design</li>
-                  </ul>
-                </Col>
-                <Col md={6}>
-                  <ul>
-                    <li>Agile Software Development</li>
-                    <li>Distributed Version Control</li>
-                    <li>Continuous Integration </li>
-                    <li>Visual Studio, NUnit, Mercurial</li>
-                    <li>MVVM, OOP</li>
-                  </ul>
-                </Col>
-              </Grid>
-              <Carousel.Caption>
-                <h3>Skill Highlights</h3>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel> */}
+        <Route render={({ location }) => (
+          <div className="App">
+            <div>
+              <Navbar className="navbar-inverse">
+                <Navbar.Header>
+                  <Navbar.Brand>
+                    Austin Spears
+                  </Navbar.Brand>
+                </Navbar.Header>
+                <Nav>
+                  <LinkContainer to="/" activeClassName="">
+                    <NavItem>Home</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/skills" activeClassName="">
+                    <NavItem>Skills</NavItem>
+                  </LinkContainer>
+                </Nav>
+              </Navbar>
+              <div>
+                <TransitionGroup>
+                  <CSSTransition
+                    key={location.key}
+                    classNames='fade'
+                    timeout={300}
+                  >
+                    <Switch location={location}>
+                      <Route exact path="/" component={Home}/>
+                      <Route path="/skills" component={Skills}/>
+                    </Switch>
+                  </CSSTransition>
+                </TransitionGroup>
+              </div>
+            </div>
           </div>
-        </div>
+        )}/>
       </BrowserRouter>
     );
   }
