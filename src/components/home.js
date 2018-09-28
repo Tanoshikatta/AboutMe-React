d3a9def2380119515ed7bb293c91efac61543003
Tanoshikatta <austinalexanderspears@gmail.com>
2018-09-28 00:26:34 -0400

    Started to add a Jumbotron to the home component

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { Jumbotron, Button } from 'react-bootstrap';
 
 class Home extends Component {
     render(){
         return(
-            <div>
-                <p>Home</p>
-            </div>
+            <Jumbotron style={{backgroundColor: "#506C7F",
+                               justifyContent: "center",
+                               margin: "50px"}}>
+                <h1>Welcome</h1>
+                <p>
+                    Software Engineer and team leader with extensive experience across the entire 
+                    application development lifecycle.  Skilled at communicating with and interfacing 
+                    between technical and non-technical parties.  Passionate about elegant algorithm 
+                    and architecture design.
+                </p>
+                <p>
+                    <Button bsStyle="primary">Learn more</Button>
+                </p>
+            </Jumbotron>
         );
     }
 }
