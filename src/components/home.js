ed33cdf470463b32718d018cde45daadc246d4ee
Tanoshikatta <austinalexanderspears@gmail.com>
2018-09-29 13:55:32 -0400

    Added a click handler to the jumbotron button that leads to my LinkedIn profile

--- src/components/home.js
+++ src/components/home.js
@@ -7,18 +7,26 @@ class Home extends Component {
             <Jumbotron style={{backgroundColor: "#506C7F",
                                justifyContent: "center",
                                margin: "50px"}}>
-                <h1>Welcome</h1>
+                <h1>Welcome!</h1>
                 <p>
-                    Software Engineer and team leader with extensive experience across the entire 
-                    application development lifecycle.  Skilled at communicating with and interfacing 
-                    between technical and non-technical parties.  Passionate about elegant algorithm 
-                    and architecture design.
+                    The url and navbar probably clued you in, but in case you were curious I'm Austin Spears.
+                    I have been developing software professionally for over six years with a primary focus on
+                    creating WPF applications using XAML and C#.
+                    <br/>
+                    <br/>
+                    I am currently the team lead for Datawatch's data preparation product: Data Prep Studio
                 </p>
                 <p>
-                    <Button bsStyle="primary">Learn more</Button>
+                    <Button bsStyle="primary" onClick={this.linkedInClick}>LinkedIn Profile</Button>
                 </p>
             </Jumbotron>
         );
+    }
+
+    linkedInClick()
+    {
+        var win = window.open("https://www.linkedin.com/in/austinspears/");
+        win.focus();
     }
 }
 
